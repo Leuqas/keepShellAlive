e30f74cc155f473fe69f461b1f877ab5f8f58240
Leuqas Yabot <yleuqas@gmail.com>
2022-03-28 07:46:44 +0800

    test

---
+++ install.js
@@ -0,0 +1,35 @@
+function exec(cmd, handler = function(error, stdout, stderr){console.log(stdout);if(error !== null){console.log(stderr)}})
+{
+  const childfork = require('child_process');
+  return childfork.exec(cmd, handler);
+}
+
+//installs
+
+//installs
+
+fs = require('fs');
+const prompt = require("prompt-sync")({ sigint: true });
+
+//bot array
+var bot = {}
+
+exec('clear')
+console.log('Welcome to the installation for the "serverOffline" script!\nthis will serve as a guide to make things simpler, lets start with setting up your discord server,\nso create a discord bot and server, and fill out the necesary informations that are asked below\n')
+token()
+//token
+const token = prompt("[installation]: whats your discord bot token?\n> ");
+console.log(`test ${token}.`);
+
+//channelId
+const channelId = prompt("[installation]: whats the id of the discord channel that you want your join logs to be at?\n> ");
+console.log(`test ${channelId}.`);
+log()
+
+function log(){
+  console.log(`token = ${bot.token}\nchannelId = ${bot.channelId}`)
+}
+//write
+// fs.writeFile('./bot/config.json', `"token": "${answer}",`, function (err) {
+//     if (err) return console.log(err);
+//   });
